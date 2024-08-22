8ceb9cbc79b3a0705d43cc5662f7ac818870337a
emily <emily@bacheconsulting.com>
2024-08-22 14:08:43 +0200

    typescript translation

---
+++ typescript/tests/fizzbuzz.spec.ts
@@ -0,0 +1,26 @@
+import {FizzBuzz} from "../src/fizzbuzz";
+
+
+describe("FizzBuzz", () => {
+  const fb = new FizzBuzz();
+
+  it("Non-multiple of three or five are preserved", () => {
+    expect(fb.convert(1)).toBe("1");
+    expect(fb.convert(4)).toBe("4");
+  });
+
+  it("Multiple of three are Fizz", () => {
+    expect(fb.convert(3)).toBe("Fizz");
+    expect(fb.convert(6)).toBe("Fizz");
+  });
+
+  it("Multiple of five are Buzz", () => {
+    expect(fb.convert(5)).toBe("Buzz");
+    expect(fb.convert(10)).toBe("Buzz");
+  });
+
+  it("Multiple of three and five are FizzBuzz", () => {
+    expect(fb.convert(15)).toBe("FizzBuzz");
+    expect(fb.convert(30)).toBe("FizzBuzz");
+  });
+});
