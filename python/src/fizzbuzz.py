df20e7466da38ce880c49208e7cf95cd564a60a8
Harry <hjwp2@cantab.net>
2025-07-07 19:59:27 +0100

    add defaulted args for customisability

--- python/src/fizzbuzz.py
+++ python/src/fizzbuzz.py
@@ -1,18 +1,24 @@
 from typing import Iterable
 
+_DEFAULT_FACTORS = {
+    3: "Fizz",
+    5: "Buzz",
+}
+
 
 def convert(
     num: int,
+    factor_names: dict[int, str] = _DEFAULT_FACTORS,
 ) -> str:
-    if num % 3 == 0 and num % 5 == 0:
-        return "FizzBuzz"
-    elif num % 3 == 0:
-        return "Fizz"
-    elif num % 5 == 0:
-        return "Buzz"
-    else:
-        return str(num)
+    factornames = "".join(
+        name for factor, name in factor_names.items() if num % factor == 0
+    )
+    return factornames or str(num)
 
 
-def for_print() -> Iterable[str]:
-    return (convert(i) for i in range(1, 101))
+def for_print(
+    start: int = 1,
+    end: int = 100,
+    factor_names: dict[int, str] = _DEFAULT_FACTORS,
+) -> Iterable[str]:
+    return (convert(i, factor_names) for i in range(1, 101))
