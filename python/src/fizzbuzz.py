1d802a11bf30f26e7f9a6ea9c64fcf1ef54d8afe
Harry <hjwp2@cantab.net>
2025-07-07 19:21:55 +0100

    type hints and ruff format

--- python/src/fizzbuzz.py
+++ python/src/fizzbuzz.py
@@ -1,4 +1,7 @@
-def convert(num):
+from typing import Iterable
+
+
+def convert(num: int) -> str:
     if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
     elif num % 3 == 0:
@@ -8,5 +11,6 @@ def convert(num):
     else:
         return str(num)
 
-def for_print():
+
+def for_print() -> Iterable[str]:
     return (convert(i) for i in range(1, 101))
--- python/tests/test_fizzbuzz.py
+++ python/tests/test_fizzbuzz.py
@@ -1,23 +1,27 @@
 import fizzbuzz
 
 
-def test_non_multiple_of_three_or_five_are_preserved():
+def test_non_multiple_of_three_or_five_are_preserved() -> None:
     assert fizzbuzz.convert(1) == "1"
     assert fizzbuzz.convert(4) == "4"
 
-def test_multiple_of_three_are_fizz():
+
+def test_multiple_of_three_are_fizz() -> None:
     assert fizzbuzz.convert(3) == "Fizz"
     assert fizzbuzz.convert(6) == "Fizz"
 
-def test_multiple_of_five_are_buzz():
+
+def test_multiple_of_five_are_buzz() -> None:
     assert fizzbuzz.convert(5) == "Buzz"
     assert fizzbuzz.convert(10) == "Buzz"
 
-def test_multiple_of_three_and_five_are_fizzbuzz():
+
+def test_multiple_of_three_and_five_are_fizzbuzz() -> None:
     assert fizzbuzz.convert(15) == "FizzBuzz"
     assert fizzbuzz.convert(30) == "FizzBuzz"
 
-def test_print():
+
+def test_print() -> None:
     all = list(fizzbuzz.for_print())
     assert len(all) == 100
     assert all[0] == "1"
