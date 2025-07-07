ee41d2ba4fcd7877d5a477373a558eb2099c206f
Harry <hjwp2@cantab.net>
2025-07-07 19:20:45 +0100

    Refactor to functions

--- python/src/fizzbuzz.py
+++ python/src/fizzbuzz.py
@@ -1,16 +1,12 @@
-class FizzBuzz:
-    def convert(self, num):
-        if num % 3 == 0 and num % 5 == 0:
-            return "FizzBuzz"
-        elif num % 3 == 0:
-            return "Fizz"
-        elif num % 5 == 0:
-            return "Buzz"
-        else:
-            return str(num)
+def convert(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
 
-    def print(self):
-        return (self.convert(i) for i in range(1, 101))
-
-if __name__ == "__main__":
-    [print(item) for item in FizzBuzz().print()]
+def for_print():
+    return (convert(i) for i in range(1, 101))
--- python/tests/test_fizzbuzz.py
+++ python/tests/test_fizzbuzz.py
@@ -1,24 +1,24 @@
-from fizzbuzz import FizzBuzz
+import fizzbuzz
 
 
 def test_non_multiple_of_three_or_five_are_preserved():
-    assert FizzBuzz().convert(1) == "1"
-    assert FizzBuzz().convert(4) == "4"
+    assert fizzbuzz.convert(1) == "1"
+    assert fizzbuzz.convert(4) == "4"
 
 def test_multiple_of_three_are_fizz():
-    assert FizzBuzz().convert(3) == "Fizz"
-    assert FizzBuzz().convert(6) == "Fizz"
+    assert fizzbuzz.convert(3) == "Fizz"
+    assert fizzbuzz.convert(6) == "Fizz"
 
 def test_multiple_of_five_are_buzz():
-    assert FizzBuzz().convert(5) == "Buzz"
-    assert FizzBuzz().convert(10) == "Buzz"
+    assert fizzbuzz.convert(5) == "Buzz"
+    assert fizzbuzz.convert(10) == "Buzz"
 
 def test_multiple_of_three_and_five_are_fizzbuzz():
-    assert FizzBuzz().convert(15) == "FizzBuzz"
-    assert FizzBuzz().convert(30) == "FizzBuzz"
+    assert fizzbuzz.convert(15) == "FizzBuzz"
+    assert fizzbuzz.convert(30) == "FizzBuzz"
 
 def test_print():
-    all = list(FizzBuzz().print())
+    all = list(fizzbuzz.for_print())
     assert len(all) == 100
     assert all[0] == "1"
     assert all[-1] == "Buzz"
